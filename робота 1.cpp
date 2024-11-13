4b70d4cee1aef007f76dde68d40f96197bc26ba3
nevrmew <147155596+nevrmew@users.noreply.github.com>
2024-11-13 03:16:47 +0300

    change file

---
+++ робота 1.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+    int number;
+    cout << "enter numbers ";
+    cin >> number;
+    int sto = number / 100;
+    int desat = (number / 10) % 10;
+    int od = number % 10;
+    int newnumber = sto * 100 + od * 10 + desat;
+    cout << "new numbers " << newnumber << endl;
+    return 0;
+}
